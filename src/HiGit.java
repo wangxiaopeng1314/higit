9920c128454fc265f8f9fb132ed3adec17abb8bd
wangxiaopeng1314 <530279459@qq.com>
2020-04-11 20:52:02 +0800

    maven下本地主分支修改

--- src/HiGit.java
+++ src/HiGit.java
@@ -2,5 +2,7 @@ public class HiGit {
     public static void main(String args[]){
         System.out.println("HiGit ");
     }
-
+    public  void bdzfz(String args[]){
+        System.out.println("maven下本地主分支修改");
+    }
 }
