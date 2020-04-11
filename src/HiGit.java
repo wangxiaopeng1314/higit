e1b3b6e67bd00f5933f8ffbbc86bdf390e1aa000
wangxiaopeng1314 <530279459@qq.com>
2020-04-11 20:36:43 +0800

    dev 分支

--- src/HiGit.java
+++ src/HiGit.java
@@ -2,5 +2,7 @@ public class HiGit {
     public static void main(String args[]){
         System.out.println("HiGit ");
     }
-
+    public void dev( ){
+        System.out.println("dev ");
+    }
 }
